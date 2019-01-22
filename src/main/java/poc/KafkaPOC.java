c79dfa9a300cd95590a8c19ec304689464908be7
Rahul Goel <rahul.goel2@delhivery.com>
2019-01-22 18:08:06 +0530

    first setup commit

---
+++ src/main/java/poc/KafkaPOC.java
@@ -0,0 +1,15 @@
+package poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableAsync
+public class KafkaPOC {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPOC.class, args);
+    }
+}
